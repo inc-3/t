4c6d6a13a1c14f6c9c97f03f849b386561356713
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-09-22 02:44:58 +0600

    Update t.py

--- t.py
+++ t.py
@@ -1,71 +1,95 @@
-from bs4 import BeautifulSoup as bs
-import requests as rs
-import time
-import os
+def filter_uids(file_path, names_to_filter):
+    # Read input data from the file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        uid_data = file.read()
 
-os.system('clear')
+    # Split the input data into lines
+    uid_list = uid_data.strip().split('\n')
 
-email = input("Email: ")
-pwd = input("Pass: ")
+    # Filter UIDs that contain any of the names in the list
+    filtered_uids = [uid for uid in uid_list if any(name in uid for name in names_to_filter)]
 
-headers = {
-    'Host': 'www.facebook.com',
-    'Cache-Control': 'max-age=0',
-    'Sec-Ch-Ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"Linux"',
-    'Upgrade-Insecure-Requests': '1',
-    'Origin': 'https://www.facebook.com',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-User': '?1',
-    'Sec-Fetch-Dest': 'document',
-    'Referer': 'https://www.facebook.com/',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-GB,en;q=0.9',
-}
+    # Return the filtered UIDs
+    return filtered_uids
 
-resp = rs.get("https://www.facebook.com/", headers=headers)
 
-soup = bs(resp.content, "html.parser")
+# Specify the file path and names to filter
+file_path = '/sdcard/2.txt'  # Replace with the path to your input file
+names_to_filter = ["Md", "Md.", "Abdul", "Akhtar", "Ali", "Amin", "Anis", "Asif", "Aziz", "Bashir", "Bilal",
+        "Faisal", "Farhan", "Habib", "Hassan", "Hossain", "Imran", "Iqbal", "Jamil", "Kamal", "Karim", 
+    "Khan", "Mahmud", "Moin", "Monir", "Nasir", "Nawaz", "Rahim", "Rashid", "Rehman", "Salim", 
+    "Sami", "Shahid", "Sharif", "Tariq", "Yasin", "Zaman", "Salman", "Wajid", "Sk", "Rahman", 
+    "Hasan", "Jahan", "Parvez", "Shakil", "Shahin", "Sohel", "Tarek", "Sajjad", "Raju", "Shamim", 
+    "Sumon", "Shahed", "Shakib", "Arif", "Sabbir", "Noman", "Shafi", "Shah", "Shahriar", "Tanvir", 
+    "Rafi", "Masud", "Rafiq", "Rakib", "Sobuj", "Shuvo", "Faruk", "Nayeem", "Mehedi", "Munna", 
+    "Anwar", "Kabir", "Shuvo", "Rajib", "Sohag", "Sajib", "Rony", "Sakib", "Shahjalal", "Nasim", 
+    "Sakil", "Shuvojit", "Sujon", "Ashik", "Shafiq", "Mahfuz", "Habibullah", "Rasel", "Mizan", 
+    "Monir", "Mithun", "Mamun", "Shanto", "Mehrab", "Rubel", "Sumon", "Sabbir", "Razib", "Khairul", 
+    "Rafat", "Arafat", "Mahbub", "Nizam", "Biplab", "Pavel", "Arman", "Bijoy", "Shanto", "Hasib", 
+    "Ranjan", "Masum", "Liton", "Shamim", "Nazim", "Tuhin", "Arman", "Forhad", "Sakif", "Jabed", 
+    "Shakir", "Rifat", "Rakib", "Muntasir", "Biplob", "Tuhin", "Rabbi", "Rab", "Sourav", "Jony", 
+    "Partha", "Pavel", "Sohan", "Ratul", "Rabiul", "Rifat", "Mizanur", "Jahid", "Alamin", "Tanjim", 
+    "Mahir", "Sajal", "Rifat", "Ratul", "Sarwar", "Shihab", "Rajon", "Jamil", "Rasel", "Ahsan", 
+    "Maruf", "Tanveer", "Sajjad", "Rifat", "Saiful", "Shohel", "Tuhin", "Anik", "Shuvo", "Ridoy", 
+    "Arafat", "Saif", "Hasib", "Bipul", "Jewel", "Sakib", "Sahed", "Sujon", "Mehedi", "Rafat", 
+    "Sakib", "Shihab", "Shamim", "Siam", "Munim", "Sharif", "Rana", "Shamim", "Rana", "Kawsar", 
+    "Mahmud", "Akash", "Rafi", "Mahbub", "Munna", "Rasel", "Mehedi", "Shohel", "Rakib", "Nayeem", 
+    "Rakib", "Mehedi", "Arafat", "Kawsar", "Tahsin", "Rasel", "Arif", "Biplob", "Rakib", "Sajjad", 
+    "Tanzim", "Tuhin", "Mehedi", "Mehedi", "Hasan", "Rasel", "Rakib", "Shamim", "Munna", "Rony", 
+    "Fahim", "Mehedi", "Jewel", "Rafsan", "Munna", "Shamim", "Najim", "Rakib", "Najim", "Sabbir", 
+    "Shanto", "Biplob", "Mithun", "Shamim", "Nazim", "Mukul", "Tahsin", "Anisur", "Abdus", 
+    "Mohammad", "Kazi", "Abul", "Nur", "Nazmul", "Sohel", "Monjur", "Arifur", "Mostafizur", 
+    "Kamrul", "Sazzad", "Aminul", "Rashed", "Mahfuzur", "Monirul", "Shafiqul", "Imtiaz", "Enamul", 
+    "Fakrul", "Mahmudul", "Samsul", "Abdur", "Farid", "Mizanur", "Jahirul", "Helal", "Shahidul", 
+    "Liton", "Rezaul", "Jashim", "Selim", "Anwar", "Motiur", "Azizul", "Golam", "Habiba", "Hanif", 
+    "Ibrahim", "Jamal", "Kabir", "Harun", "Masud", "Mahim", "Jisan", "Jibon", "Shofik", "Mustakim", 
+    "Irfan", "Rahmatullah", "Fazlul", "Shamsul", "Afsar", "Mahbubul", "Shafayat", "Shariful", 
+    "Iqra", "Anan", "Ishtiaq", "Ahad", "Ehsanul", "Abdullah", "Shabbir", "Liton", "Masum", 
+    "Aklas", "Ashraful", "Atiq", "Mohiuddin", "Najmul", "Feroze", "Jaber", "Ashrafuzzaman", 
+    "Mahbubur", "Nazib", "Sadiq", "Shahedul", "Imranul", "Muntashir", "Naushad", "Rashidur", "Touhid", 
+    "Wali", "Mahfuzul", "Saad", "Azhar", "Naeem", "Shahiduzzaman", "Tazul", "Helaluddin", "Masudul", 
+    "Murshed", "Nure Alam", "Nasrul", "Khalid", "Sayeed", "Maswood", "Shamsuzzaman", "Tanzir", 
+    "Nahid", "Shaikh", "Rubayet", "Shadman", "Hasnain", "Shams", "Ibadur", "Arafatul", "Humaun", 
+    "Akash", "Tarik", "Shuvo", "Arshad", "Bashar", "Junaid", "Khaled", "Mahin", "Nadir", "Wasim", 
+    "Zubair", "Nazrul", "Rashedul", "Zakir", "Aftab", "Anwarul", "Asad", "Fazle", "Khorshed", 
+    "Lokman", "Ishraq", "Touhidur", "Hassanuzzaman", "Nusratullah", "Tayef", "Tawfiq", "Zahedul", 
+    "Shojib", "Abdulahi", "Shawkat", "Sadat", "Zahidur", "Hossainul", "Adnan", "Tarek", "Saeed",
+    "Sk", "Amin", "Emran", "Rana", "Riad", "Shagor", "Kawsar", "Anis", "Ashraf", "Akbar", "Alim", 
+    "Arif", "Azim", "Bashir", "Babar", "Badrul", "Baki", "Belal", "Bodi", "Bulbul", "Chowdhury", "Daud", "Dulal", 
+    "Emon", "Enamul", "Fahim", "Farhad", "Faisal", "Ferdous", "Gafur", "Gazi", "Habib", "Hasib", "Hasan", "Helal", 
+    "Hossain", "Ibrahim", "Imran", "Iqbal", "Ismail", "Jahid", "Jalal", "Jamil", "Jasim", "Kabir", "Kamal", "Karim", 
+    "Khaled", "Liton", "Mahi", "Mahfuz", "Mamun", "Manik", "Masud", "Mizan", "Mokbul", "Moin", "Monir", "Mostafa", 
+    "Mujib", "Murad", "Nafis", "Naim", "Nasim", "Nazmul", "Niaz", "Noman", "Nur", "Parvez", "Rafiqul", "Rakib", 
+    "Rashed", "Razib", "Rezaul", "Sohel", "Riaz", "Ripon", "Rubel", "Sabbir", "Sadiq", "Sajid", "Salam", "Sami", 
+    "Sanjib", "Shahid", "Shakib", "Shamsul", "Sharif", "Sohag", "Sumon", "Tariq", "Taufiq", "Touhid", "Wahid", 
+    "Yasin", "Zahid", "Zahir", "Zakir", "Zaman", "Zia", "Zubair", "Abdul", "Abid", "Adnan", "Afif", "Ahad", "Ahmad", 
+    "Ahsan", "Akash", "Ali", "Alim", "Aminul", "Amzad", "Anik", "Anwar", "Arifur", "Asad", "Ashfaq", "Atiq", "Azhar", 
+    "Aziz", "Babu", "Badal", "Basir", "Bijoy", "Bilal", "Chanchal", "Delwar", "Dipu", "Ehsan", "Ershad", "Eshan", 
+    "Fakhrul", "Faruq", "Fazle", "Golam", "Hafiz", "Hamid", "Haris", "Hasnat", "Hedayet", "Humayun", "Ilias", 
+    "Ishtiaq", "Jabed", "Jafor", "Jamshed", "Johirul", "Junaid", "Kamrul", "Kawsar", "Khalil", "Khorshed", "Lalon", 
+    "Latif", "Mahmud", "Majid", "Maksud", "Manjur", "Maruf", "Mazhar", "Mehedi", "Milton", "Minhaj", "Miraz", 
+    "Mobarak", "Mohsin", "Morshed", "Muazzam", "Mubin", "Mujibur", "Mustafa", "Nadim", "Nahid", "Nasir", "Nazim", 
+    "Niloy", "Nizam", "Noor", "Nure", "Obaid", "Omar", "Pavel", "Rahim", "Rakhat", "Rana", "Rasel", "Rifat", "Riyad", 
+    "Rokon", "Ruhul", "Sabit", "Saiful", "Sajjad", "Salim", "Sarwar", "Shafiq", "Shahin", "Shakil", "Shamim", 
+    "Shanto", "Sheikh", "Sohail", "Subho", "Tanjil", "Tanvir", "Tipu", "Tuhin", "Uzzal", "Wasim", "Younus", "Zafor", 
+    "Zahidul", "Zakaria", "Zamal", "Zulfiqar", "Afsar", "Afzal", "Alam", "Amirul", "Ananta", "Anindya", "Ashik", 
+    "Ashim", "Ashraful", "Atikur", "Aynal", "Azad", "Babul", "Bahauddin", "Barkat", "Bashar", "Bokul", "Danish", 
+    "Ekram", "Fahad", "Faisal", "Farhan", "Farid", "Fayez", "Ferdous", "Gaziul", "Giyas", "Golam", "Harun", 
+    "Hasanuzzaman", "Helal", "Himel", "Hiron", "Hossainul", "Hridoy", "Ibrahim", "Iftekhar", "Imtiaz", "Iqbal", 
+    "Ishtiaque", "Jalil", "Jewel", "Jibon", "Kamal", "Kamrul", "Kawser", "Kazi", "Khaledur", "Latifur", "Liton", 
+    "Lutfar", "Mahadi", "Mahbub", "Mahdi", "Majed", "Manik", "Maruf", "Masraf", "Masum", "Mazharul", "Mehdi", 
+    "Milon", "Mizanur", "Mohammad", "Mohibur", "Moinul", "Mokhlesur", "Monowar", "Morsalin", "Mubarak", "Muhaimin", 
+    "Mujahid", "Mustafizur", "Nadim", "Nahiyan", "Najmul", "Nayeem", "Nazir", "Nezam", "Nishat", "Niyaz", "Noorul", 
+    "Oli", "Omar", "Parvez", "Rabbani", "Rabbi", "Rafique", "Raihan", "Rakibul", "Rashedul", "Reza", "Riazul", 
+    "Rokonuzzaman", "Ruhul", "Rumana", "Saad", "Sabbir", "Sabir", "Sadi", "Saiful", "Sajeeb", "Sajidur", "Sajjadur", 
+    "Salman", "Samad", "Samir", "Sanjoy", "Sarwar", "Saud", "Sazzad", "Shahidul", "Shahed", "Shahedul", "Shahriar", 
+    "Shakibul", "Shamimul", "Shariful", "Shaukat", "Sohail", "Sujan", "Sumon", "Suruj", "Syed", "Tajul", "Tamal", 
+    "Tanveer", "Tariqul", "Taufiqur", "Tipu", "Toufique", "Touhidul", "Uzzal", "Wasim", "Yasinul", "Yunus", 
+    "Zahirul", "Zakariah", "Zayed", "Ziaul", "Zillur", "Zubayer", "Zulfiqar"]
 
-form = soup.find('form', action=True, method="post")
+# Call the function to filter UIDs
+filtered_uids = filter_uids(file_path, names_to_filter)
 
-url = "https://www.facebook.com" + form["action"]
-
-jazoest = form.find('input', {'name': 'jazoest'})['value']
-lsd = form.find('input', {'name': 'lsd'})['value']
-
-_js_datr = resp.text.split('_js_datr","')[1].split('""')[0]
-
-cookies = resp.cookies.get_dict()
-
-cookies_final = {
-    "_js_datr": _js_datr,
-    "wd": "873x850",
-    "dpr": "1.100000023841858"
-}
-
-cookies_final.update(cookies)
-
-body = {
-    "jazoest": jazoest,
-    "lsd": lsd,
-    "email": email,
-    "login_source": "comet_headerless_login",
-    "next": "",
-    "encpass": f"#PWD_BROWSER:0:{round(time.time())}:{pwd}"
-}
-
-resp = rs.post(url, cookies=cookies_final, headers=headers, data=body, allow_redirects=False)
-
-cookies.update(resp.cookies.get_dict())
-
-cookie_list = [f"{key}={value}" for key, value in cookies.items()]
-
-cookie = "; ".join(cookie_list)
-
-print(cookie)
+# Print the filtered UIDs
+for uid in filtered_uids:
+    print(uid)
